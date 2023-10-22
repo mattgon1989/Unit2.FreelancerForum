f52509a92e129d2afd042f422133b6746daf4537
matt gonzalez <mattgon@matts-MacBook-Air.local>
2023-10-21 18:04:37 -0700

    added code to js and html

--- index.js
+++ index.js
@@ -23,8 +23,164 @@ New freelancers continue to appear every few seconds, and the average starting p
 
 */
 
+
+// array of freelancers names, price, and occupations.
 const freelancers = [
-    { name: "Alice", price: 30, occupation: "writer" },
-    { name: "Bob", price: 50, occupation: "teacher" },
-    { name: "Carol", price: 70, occupation: "Programmer" },
-]
+    { name: 'Alice', price: 30, occupation: 'Writer' },
+    { name: 'Bob', price: 50, occupation: 'Teacher' },
+    { name: 'Carol', price: 70, occupation: 'Programmer' },
+];
+
+renderPage();
+
+// add a new random freelancer to the freelancers array and
+// render the page once every 5 seconds
+setInterval(() => {
+    pushRandomFreelancer();
+    renderPage();
+}, 5000);
+
+// pushRandomFreelancer creates a new random freelancer object
+// and pushes it onto the end of the freelancers array.
+function pushRandomFreelancer() {
+    const randomFreelancer = makeRandomFreelancer();
+    freelancers.push(randomFreelancer);
+}
+
+// averageStartingPrice averages all prices from the
+// freelancers array and returns that number
+function averageStartingPrice() {
+    let total = 0;
+    for (let i = 0; i < freelancers.length; i++) {
+        total += freelancers[i].price;
+    }
+    return (total / freelancers.length).toFixed(2);
+}
+
+// renderStartingPrice updates the average-starting-price DOM element
+// to be the most up-to-date average starting price across the freelancers
+// array.
+function renderStartingPrice() {
+    const priceSpan = document.querySelector('#average-starting-price');
+    if (priceSpan != null) {
+        priceSpan.innerText = averageStartingPrice();
+    }
+}
+
+// renderFreelancersTable will update the DOM table rows with updated
+// freelancers information
+function renderFreelancersTable() {
+    const titleRow = titlesTableRow();
+    
+    const tableBody = document.querySelector('tbody');
+    // clear the existing table body
+    tableBody.innerHTML = '';
+    tableBody.appendChild(titleRow);
+
+    // append each freelancer as a table row
+    for (let i = 0; i < freelancers.length; i++) {
+        const freelancerRow = freelancerTableRow(freelancers[i]);
+        tableBody.appendChild(freelancerRow);
+    }
+}
+
+// titlesTableRow creates a DOM table row of the table titles
+function titlesTableRow() {
+    const titleRow = document.createElement('tr');
+
+    // name column
+    const nameCol = document.createElement('td');
+    nameCol.innerText = 'Name';
+    titleRow.appendChild(nameCol);
+
+    // occupation column
+    const occupationCol = document.createElement('td');
+    occupationCol.innerText = 'Occupation';
+    titleRow.appendChild(occupationCol);
+
+    // starting price column
+    const startingPriceCol = document.createElement('td');
+    startingPriceCol.innerText = 'Starting Price';
+    titleRow.appendChild(startingPriceCol);
+
+    return titleRow;
+}
+
+// freelancerTableRow takes a freelancer and returns a
+// DOM table row of it
+function freelancerTableRow(freelancer) {
+    const freelancerRow = document.createElement('tr');
+
+    // name column
+    const nameCol = document.createElement('td');
+    nameCol.innerText = freelancer.name;
+    freelancerRow.appendChild(nameCol);
+
+    // occupation column
+    const occupationCol = document.createElement('td');
+    occupationCol.innerText = freelancer.occupation;
+    freelancerRow.appendChild(occupationCol);
+
+    // starting price column
+    const startingPriceCol = document.createElement('td');
+    startingPriceCol.innerText = freelancer.price;
+    freelancerRow.appendChild(startingPriceCol);
+
+    return freelancerRow;
+}
+
+// renderPage updates DOM elements across the page to be consistent
+// with the latest freelancers array entries.
+function renderPage() {
+    renderStartingPrice();
+    renderFreelancersTable();
+}
+
+const freelancerNames = [
+    'Alice',
+    'Bob',
+    'Carol',
+    'Casey',
+    'Matt',
+    'Adam',
+    'John',
+    'Jake',
+    'Andrew',
+    'Mark',
+    'Kevon',
+    'Yvonne',
+];
+
+const freelancerOccupations = [
+    'Gardener',
+    'Programmer',
+    'Writer',
+    'Teacher',
+    'Actor',
+    'Digital Artist',
+    'Guitarist',
+    'Keyboardist',
+    'Drummer',
+    'Singer',
+    'Songwriter',
+];
+
+// makeRandomFreelancer returns a Freelancer object.
+// It picks a random entry from names and occupations and generates a random price.
+function makeRandomFreelancer() {
+    const name = freelancerNames[randomIndex(freelancerNames)];
+    const occupation = freelancerOccupations[randomIndex(freelancerOccupations)];
+    const price = Math.floor(Math.random() * 100);
+
+    return {
+        name,
+        occupation,
+        price,
+    };
+}
+
+// randomIndex takes an array as input and returns a
+// random number from 0 to arr.length-1.
+function randomIndex(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
